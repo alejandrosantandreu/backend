61c0f64bdeacddbc4b204d6883fc1352dd04adb2
Alejandro S <alejandro.santandreu@estudiantat.upc.edu>
2023-07-31 20:21:39 +0200

    Post correction

--- src/quality-factors/quality-factors.module.ts
+++ src/quality-factors/quality-factors.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { QualityFactorsService } from './quality-factors.service';
 import { QualityFactorsController } from './quality-factors.controller';
 import { HttpModule } from '@nestjs/axios';
+import { QualityFactor } from './entities/quality-factor.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +11,7 @@ import { HttpModule } from '@nestjs/axios';
       timeout: 5000,
       maxRedirects: 5,
     }),
+    TypeOrmModule.forFeature([QualityFactor])
   ],
   controllers: [QualityFactorsController],
   providers: [QualityFactorsService]
--- src/strategic-indicators/strategic-indicators.module.ts
+++ src/strategic-indicators/strategic-indicators.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { StrategicIndicatorsService } from './strategic-indicators.service';
 import { StrategicIndicatorsController } from './strategic-indicators.controller';
 import { HttpModule } from '@nestjs/axios';
+import { StrategicIndicator } from './entities/strategic-indicator.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +11,7 @@ import { HttpModule } from '@nestjs/axios';
       timeout: 5000,
       maxRedirects: 5,
     }),
+    TypeOrmModule.forFeature([StrategicIndicator])
   ],
   controllers: [StrategicIndicatorsController],
   providers: [StrategicIndicatorsService]
