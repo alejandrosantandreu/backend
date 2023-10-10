a457d11c71c0b4a8867968e0685019507f88783c
Alejandro S <alejandro.santandreu@estudiantat.upc.edu>
2023-10-10 16:31:54 +0200

    update

--- src/metrics/dto/create-metric.dto.ts
+++ src/metrics/dto/create-metric.dto.ts
@@ -1,14 +1,10 @@
 export class CreateMetricDto {
-    id: number;
     name: string;
     description: string;
     value: number;
     value_description: number;
     date: Date;
-    datasource: string;
     rationale: string;
-    confidence80: string;
-    confidence95: string;
-    forecastingError: string;
     qualityFactors: Array<string>;
+    project: string;
 }
--- src/metrics/entities/metric.entity.ts
+++ src/metrics/entities/metric.entity.ts
@@ -1,29 +1,52 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
+export class ColumnNumericTransformer {
+    to(data: number): number {
+        return data;
+    }
+    from(data: string): number {
+        return parseFloat(data);
+    }
+}
+
 @Entity()
 export class Metric {
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     name: string;
+
     @Column()
     description: string;
-    @Column()
+
+    @Column('numeric', {
+        nullable: false,
+        precision: 3, 
+        scale: 2, 
+        default: 0.0,
+        transformer: new ColumnNumericTransformer(),
+    })
     value: number;
-    @Column()
+
+    @Column('numeric', {
+        nullable: false,
+        precision: 6, 
+        scale: 2, 
+        default: 0.0,
+        transformer: new ColumnNumericTransformer(),
+    })
     value_description: number;
+
     @Column()
     date: Date;
-    @Column()
-    datasource: string;
+
     @Column()
     rationale: string;
-    @Column({ nullable: true })
-    confidence80: string;
-    @Column({ nullable: true })
-    confidence95: string;
-    @Column()
-    forecastingError: string;
+
     @Column('text', { array: true })
     qualityFactors: string[];
+
+    @Column()
+    project: string;
 }
